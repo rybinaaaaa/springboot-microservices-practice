cccaf496f4e5543ca012f69c6c63b22b409d6d95
rybinali <rybinaaaaa.a@gmail.com>
2024-06-20 11:05:21 +0200

    add retry mechanismus

--- order-service/src/main/java/com/rybina/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/rybina/order_service/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.rybina.order_service.controller;
 import com.rybina.order_service.dto.OrderCreateDto;
 import com.rybina.order_service.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,15 +25,19 @@ public class OrderController {
 //    "inventory" = name, which we wrote to the application.yaml -- resilience4j.circuitbreaker.instances.inventory; etc: resilience4j.circuitbreaker.instances.some_namelalala
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
 
-/*
+    /*
     Аннотация @TimeLimiter из библиотеки Resilience4j
     предназначена для работы с асинхронными методами и в
     озвращаемыми типами, такими как CompletableFuture.
     Она не может быть применена к синхронным методам.
     Если метод будет выполняться синхронно, TimeLimiter не сможет
     ограничить время его выполнения.
- */
+    */
     @TimeLimiter(name = "inventory") //to limit time of proceeding the request
+    /*
+    @Retry аннотация повтряет запрос еще [retry.instances.inventory.max-attempts: 3] раза перед тем как вернет 500 статус (если все разы время превысило лимиты)
+     */
+    @Retry(name = "inventory")
     public CompletableFuture<String> create(@RequestBody OrderCreateDto orderCreateDto) {
 
 //        log.info("Order has been successfully created");
